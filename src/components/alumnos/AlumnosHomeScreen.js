e6bf5493d8bca4d61297764efa986e8d6a99d36d
Tomi <tomicaceres07@gmail.com>
2022-06-28 17:55:09 -0300

    Section Board finished. Separator added.

--- src/components/alumnos/AlumnosHomeScreen.js
+++ src/components/alumnos/AlumnosHomeScreen.js
@@ -19,12 +19,44 @@ export const AlumnosHomeScreen = () => {
   
   return (
     <div>
-      <section id='home'>
+      <section id="home">
         <h1 id="title"><span id="title-p1">Liceo Militar</span><span id="title-p2">General Paz</span></h1>
         <h2 id="subtitle">Sección Alumnos</h2>
       </section>
-      <section id='board'>
+      <section id="board">
         <h2 id="board-title">Tablero</h2>
+        <div id="board-week">
+          <div id="board-padding">
+              <h4 className="board-name">Lunes</h4>
+              <p className="board-task">Examen Matemática</p>
+              <h4 className="board-name">Martes</h4>
+              <p className="board-task">Acto de Malvinas</p>
+              <h4 className="board-name">Miércoles</h4>
+              <p className="board-task">Feriado</p>
+              <h4 className="board-name">Jueves</h4>
+              <p className="board-task">Visita a la UCC</p>
+              <h4 className="board-name">Viernes</h4>
+              <p id="last-task">Presentación informática</p>
+          </div>
+        </div>
+      </section>
+      <div className="separator"></div>
+      <section id="board">
+        <h2 id="board-title">Tablero</h2>
+        <div id="board-week">
+          <div id="board-padding">
+              <h4 className="board-name">Lunes</h4>
+              <p className="board-task">Examen Matemática</p>
+              <h4 className="board-name">Martes</h4>
+              <p className="board-task">Acto de Malvinas</p>
+              <h4 className="board-name">Miércoles</h4>
+              <p className="board-task">Feriado</p>
+              <h4 className="board-name">Jueves</h4>
+              <p className="board-task">Visita a la UCC</p>
+              <h4 className="board-name">Viernes</h4>
+              <p id="last-task">Presentación informática</p>
+          </div>
+        </div>
       </section>
     </div>
   );
