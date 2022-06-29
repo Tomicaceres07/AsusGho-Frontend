92735bd5ec8bd3aedea2162a28553baf35c64522
Tomi <tomicaceres07@gmail.com>
2022-06-29 14:30:57 -0300

    AlumnosPreviasScreen Done. _base.scss modified. Routes added. Buttons funcionality missing

--- src/routers/alumnos/AlumnosDashboardRoutes.js
+++ src/routers/alumnos/AlumnosDashboardRoutes.js
@@ -1,7 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 
-import { AlumnosHomeScreen } from 'components/alumnos/AlumnosHomeScreen';
 import { AlumnosNavBar } from 'components/alumnos/AlumnosNavBar';
+import { AlumnosHomeScreen } from 'components/alumnos/AlumnosHomeScreen';
+import { AlumnosPreviasScreen } from 'components/alumnos/AlumnosPreviasScreen';
 import { AlumnosFooter } from 'components/alumnos/AlumnosFooter';
 
 export const AlumnosDashboardRoutes = () => {
@@ -11,6 +12,7 @@ export const AlumnosDashboardRoutes = () => {
       <Routes>
         {/* <Route path="/asistencia" element={<AlumnosHomeScreen />} /> */}
         <Route path="/alumnos" element={<AlumnosHomeScreen />} />
+        <Route path="/previas" element={<AlumnosPreviasScreen />} />
         {/* <Route path="/login" element={<AlumnosHomeScreen />} /> */}
         {/* <Route path="/*" element={<AlumnosDashboardRoutes />} /> */}
 
